384fda80091b3728ac7f695dd0bbb339fa9e19e0
Sebastien Toscano <sebastiengtoscano@gmail.com>
2025-04-05 20:28:33 +0200

    Remove base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/my-portfolio/',  // 👈 add this line
   plugins: [react()],
 });
